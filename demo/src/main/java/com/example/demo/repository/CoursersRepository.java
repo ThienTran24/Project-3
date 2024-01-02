b0e290f2902229057130b45a9d3bbd1c100658e7
ThienTQ <thien.tq183992@sis.hust.edu.vn>
2024-01-02 21:56:10 +0700

    update

--- demo/src/main/java/com/example/demo/repository/CoursersRepository.java
+++ demo/src/main/java/com/example/demo/repository/CoursersRepository.java
@@ -37,36 +37,36 @@ public interface CoursersRepository extends CrudRepository<Courses, Long> {
 	public List<Courses> findByName(@Param("name") String name);
 	
 	@Query(value = "select d.* from public.courses d where (1=1 or (d.mastercategories Like :category or d.categories Like :category)) "
-			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :start)) "
-			+ "and (1=1 or (CAST(d.duration As Decimal) >= :botDur and CAST(d.duration As Decimal) <= :topDur)) "
+			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :stars)) "
+			+ "and (1=1 or ((CAST(d.duration As Decimal) >= :botDur) and (CAST(d.duration As Decimal) <= :topDur))) "
 			+ "order by case when d.numReviews Like 'NaN' then 1 else 0 end, "
 			+ "CAST(replace(d.numReviews, ',', '') As Decimal)*CAST(replace(d.stars, ',', '') As Decimal) DESC "
 			+ "OFFSET :nextRow ROWS "
 			+ "FETCH FIRST :numRow ROW ONLY ", nativeQuery = true)
 	public List<Courses> getCourseFilter(@Param("category") String category,
-			@Param("name") String name, @Param("start") Double start,
+			@Param("name") String name, @Param("stars") Double stars,
 			@Param("botDur") Double botDur, @Param("topDur") Double topDur
 			,@Param("nextRow") int nextRow, @Param("numRow") int numRow);
 	
 	@Query(value = "select d.* from public.courses d where (1=1 or (d.mastercategories Like :category or d.categories Like :category)) "
-			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :start)) "
+			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :stars)) "
 			+ "and (1=1 or (CAST(d.duration As Decimal) >= :topDur)) "
 			+ "order by case when d.numReviews Like 'NaN' then 1 else 0 end, "
 			+ "CAST(replace(d.numReviews, ',', '') As Decimal)*CAST(replace(d.stars, ',', '') As Decimal) DESC "
 			+ "OFFSET :nextRow ROWS "
 			+ "FETCH FIRST :numRow ROW ONLY ", nativeQuery = true)
 	public List<Courses> getCourseFilterTop(@Param("category") String category,
-			@Param("name") String name, @Param("start") Double start, @Param("topDur") Double topDur
+			@Param("name") String name, @Param("stars") Double stars, @Param("topDur") Double topDur
 			,@Param("nextRow") int nextRow, @Param("numRow") int numRow);
 	
 	@Query(value = "select d.* from public.courses d where (1=1 or (d.mastercategories Like :category or d.categories Like :category)) "
-			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :start)) "
+			+ "and (1=1 or (d.name Like ('%' || :name || '%'))) and (1=1 or (CAST(d.stars As Decimal) >= :stars)) "
 			+ "and (1=1 or (CAST(d.duration As Decimal) <= :botDur)) "
 			+ "order by case when d.numReviews Like 'NaN' then 1 else 0 end, "
 			+ "CAST(replace(d.numReviews, ',', '') As Decimal)*CAST(replace(d.stars, ',', '') As Decimal) DESC "
 			+ "OFFSET :nextRow ROWS "
 			+ "FETCH FIRST :numRow ROW ONLY ", nativeQuery = true)
 	public List<Courses> getCourseFilterBot(@Param("category") String category,
-			@Param("name") String name, @Param("start") Double stars,@Param("botDur") Double botDur
+			@Param("name") String name, @Param("stars") Double stars,@Param("botDur") Double botDur
 			,@Param("nextRow") int nextRow, @Param("numRow") int numRow);
 }
